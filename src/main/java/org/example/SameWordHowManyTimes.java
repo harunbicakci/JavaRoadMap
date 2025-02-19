d14acfd198f5ec00bb1d4034bb430eb431815d83
Mad Triathlete <h_bicakci@msn.com>
2025-02-19 08:36:53 -0500

    project start

---
+++ src/main/java/org/example/SameWordHowManyTimes.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class SameWordHowManyTimes {
+
+    public static void calcutateWords(String givenString){
+
+    }
+
+
+
+    public static void main(String[] args){
+
+        String givenString = "The quick brown fox jumped under the nearby tree. What the fox did next surprised the crowd. The silence was followed by the applause. Applause that lasted for minutes. The fox blushed under the tree.";
+
+    }
+
+
+}
