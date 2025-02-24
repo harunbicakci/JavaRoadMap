cf3133a6e306f808b1b3dbec8e43edab31fdca30
Mad Triathlete <h_bicakci@msn.com>
2025-02-23 23:30:19 -0500

    find largest, smallest and second largest numbers in an array added

--- src/main/java/org/example/BrokenLinksFinder.java
+++ src/main/java/org/example/BrokenLinksFinder.java
@@ -5,38 +5,38 @@ import java.net.URL;
 
 public class BrokenLinksFinder {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com");
-
-        // This is VALID METHOD
-
-        // Get all anchor tags
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println("Total links: " + links.size());
-
-        for(WebElement link : links){
-
-            String url = link.getAttribute("href");
-            if(url == null || url.isEmpty()){
-                System.out.println("URL is eighter not configurated for anchor tag or is empty");
-                continue;
-            }
-            try{
-                URL linkUrl = new URL(url);
-                HttpURLConnection httpConn = (HttpURLConnection) linkUrl.openConnection();
-                httpConn.setConnectTimeout(3000);
-                httpConn.connect();
-                int responseCode = httpConn.getResponseCode();
-                if( responseCode >= 400){
-                    System.out.println(url + " is a broken link. Response Code: " + responseCode);
-                }else{
-                    System.out.println(url + " is a valid link. Response Code: " + responseCode);
-                }
-            } catch(Exception e){
-                System.out.println("Exception occured for URL: " + url + " - " + e.getMessage());
-            } finally {
-                driver.quit();
-            }
-        }
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.amazon.com");
+//
+//        // This is VALID METHOD
+//
+//        // Get all anchor tags
+//        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        System.out.println("Total links: " + links.size());
+//
+//        for(WebElement link : links){
+//
+//            String url = link.getAttribute("href");
+//            if(url == null || url.isEmpty()){
+//                System.out.println("URL is eighter not configurated for anchor tag or is empty");
+//                continue;
+//            }
+//            try{
+//                URL linkUrl = new URL(url);
+//                HttpURLConnection httpConn = (HttpURLConnection) linkUrl.openConnection();
+//                httpConn.setConnectTimeout(3000);
+//                httpConn.connect();
+//                int responseCode = httpConn.getResponseCode();
+//                if( responseCode >= 400){
+//                    System.out.println(url + " is a broken link. Response Code: " + responseCode);
+//                }else{
+//                    System.out.println(url + " is a valid link. Response Code: " + responseCode);
+//                }
+//            } catch(Exception e){
+//                System.out.println("Exception occured for URL: " + url + " - " + e.getMessage());
+//            } finally {
+//                driver.quit();
+//            }
+//        }
     }
 }
---
+++ src/main/java/org/example/FindMaxAndMinNumbersInAnArray.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class FindMaxAndMinNumbersInAnArray {
+
+    public static void main(String[] args) {
+        int[] arr = {12, 23, 41, 73, 13, 2, 98, 94, 35};
+
+        System.out.println("The largest number in thi array is --> " + findMaxNumber(arr));
+        System.out.println("The smallest number in thi array is --> " + findMinNumber(arr));
+        System.out.println("The second largest number in thi array is --> " + findSecondMaxNumber(arr));
+    }
+
+    public static int findMaxNumber(int[] arr){
+        int maxNo = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > maxNo){
+                maxNo = arr[i];
+            }
+        }
+        return maxNo;
+    }
+
+    public static int findMinNumber(int[] arr){
+        int minNo = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < minNo){
+                minNo = arr[i];
+            }
+        }
+        return minNo;
+    }
+
+    public static int findSecondMaxNumber(int[] arr){
+        int maxNo = arr[0];
+        int secondMaxNo = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > maxNo){
+                secondMaxNo = maxNo;
+                maxNo = arr[i];
+            }else if(arr[i] > secondMaxNo && arr[i] != maxNo) {
+                secondMaxNo = arr[i];
+            }
+        }
+        return secondMaxNo;
+    }
+
+}
+
